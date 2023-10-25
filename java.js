e61423e2f03a83f721a69438afab97fcb9ebdc97
Thens <96973546+Thens18@users.noreply.github.com>
2023-10-25 16:16:56 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,33 @@
+const add= ()=>
+{
+   const a=document.getElementById("a").value;
+   const b=document.getElementById("b").value;
+   function ad()
+   {
+      let c=a+b;
+      return c;
+   }
+   console.log(ad());
+   console.log(typeof(ad))
+}
+const sub= ()=>
+{
+   const a=document.getElementById("a").value;
+   const b=document.getElementById("b").value;
+   const c=a-b;
+   console.log(c);
+}
+const mul= ()=>
+{
+   const a=document.getElementById("a").value;
+   const b=document.getElementById("b").value;
+   const c=a*b;
+   console.log(c);
+}
+const div= ()=>
+{
+   const a=document.getElementById("a").value;
+   const b=document.getElementById("b").value;
+   const c=ab;
+   console.log(c);
+}
